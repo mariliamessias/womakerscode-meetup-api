b682ab446f8650d638e9e2143661c0ce11ab2b47
mariliamessias <marilia.oliveira@outlook.com>
2022-03-29 19:37:39 -0300

    first commit

---
+++ src/test/java/com/womakerscode/meetup/MeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.womakerscode.meetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
